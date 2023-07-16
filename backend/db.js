61673f80aeaba919eaea429f98123699ca1a6f37
SUBHAM SABAT <78680373+SUBHAMSABAT2001@users.noreply.github.com>
2023-07-16 16:19:29 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoURI= "mongodb+srv://root:root@cluster0.cnxqsip.mongodb.net/inotebook1"
+const mongoURI= "mongodb+srv://root123:root1234@cluster0.cnxqsip.mongodb.net/inotebook1"
 
 const connectToMongo = ()=>{
     mongoose.connect(mongoURI, ()=>{
